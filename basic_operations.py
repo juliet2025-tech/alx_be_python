ff5f4da3c8cc0c80fea4356a2cc310c530748bef
ojide chibundo <chibundojuliet21@gmail.com>
2025-11-15 10:10:27 +0100

    Add basic_operations.py with arithmetic operations

---
+++ basic_operations.py
@@ -0,0 +1,13 @@
+# Define the numbers
+number1 = 10
+number2 = 5
+
+# Perform operations
+sum_result = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+
+# Print results
+print("Addition of", number1, "and", number2, "is", sum_result)
+print("Subtraction of", number1, "and", number2, "is", difference)
+print("Multiplication of", number1, "and", number2, "is", product)
