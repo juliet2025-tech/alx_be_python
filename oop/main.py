11276d8e45f96a6353b4115166d372204d6eafa8
ojide chibundo <chibundojuliet21@gmail.com>
2025-12-14 22:23:40 +0100

    update on oop

--- oop/main.py
+++ oop/main.py
@@ -1,8 +1,18 @@
-from book_class import Book
+from library_system import Book, EBook, PrintBook, Library
 
-my_book = Book("1984", "George Orwell", 1949)
+def main():
+    my_library = Library()
 
-print(my_book)        # __str__
-print(repr(my_book))  # __repr__
+    classic_book = Book("Pride and Prejudice", "Jane Austen")
+    digital_novel = EBook("Snow Crash", "Neal Stephenson", 500)
+    paper_novel = PrintBook("The Catcher in the Rye", "J.D. Salinger", 234)
 
-del my_book           # __del__
+    my_library.add_book(classic_book)
+    my_library.add_book(digital_novel)
+    my_library.add_book(paper_novel)
+
+    my_library.list_books()
+
+if __name__ == "__main__":
+    main()
+
